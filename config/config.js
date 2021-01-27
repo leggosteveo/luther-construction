c95d375cbe901e7c7124b6030f06b471e75ab658
Stephen Tyler <user@Stephens-MacBook-Air.local>
2021-01-26 22:04:31 -0500

    fixed PORT issue

--- config/config.js
+++ config/config.js
@@ -2,5 +2,5 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL;
-exports.PORT = 8080 || process.env.PORT;
+exports.PORT = process.env.PORT || 8080;
 exports.jwtSecret = process.env.jwtSecret;
